5ba190b26f0b1535d5f35e8d2c1af7c8857c88ad
RaffGremory <112942676+RaffGremory@users.noreply.github.com>
2022-09-20 15:03:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -119,8 +119,8 @@ global.author = '                「 WH-MODS-BOT-V1  あ」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://i.ibb.co/XZrK6yQ/transformers.jpg' //Main Thumbnail
-global.imagebot = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
+global.thumb = 'https://ibb.co/1vJ2nTy' //Main Thumbnail
+global.imagebot = 'https://ibb.co/1vJ2nTy'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
 global.thumbs = ['https://i.ibb.co/XZrK6yQ/transformers.jpg']
 global.fla = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
